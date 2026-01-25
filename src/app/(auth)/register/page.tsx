8ade5f59901d878b00b7b0f2b3af64a85d6f893c
DheerajTha <dk578206.@gmail.com>
2026-01-25 12:05:12 +0530

    register ui work

---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,12 @@
+import WelcomePage from '@/Components/Welcome'
+import React from 'react'
+
+const Register = () => {
+  return (
+    <div>
+        <WelcomePage />
+    </div>
+  )
+}
+
+export default Register
